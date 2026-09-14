564a6a381dac08b50d0062b1cae6f35b00f1c0da
P U S SATYA AMBICA PRASAD <119142334+umeshsrisatya@users.noreply.github.com>
2026-09-15 00:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (98.27%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& a, vector<int>& b) {
+        vector<int> temp;
+
+        if ((b[0] < a[2] && b[1] < a[3]) && (b[2] > a[0] && b[3] > a[1])) {
+            return true;
+        }
+        return false;
+    }
+};
